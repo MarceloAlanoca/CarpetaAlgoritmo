31d4af4930a2f03ee6165106d2a4fed785d2a23a
El_GG_315 <elfalopade2024@gmail.com>
2026-06-12 11:31:29 -0300

    Subiendo act 17 (falta terminar)

---
+++ Act-N°17-Martin-Alanoca/Ejercicio1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class PersonaGimnasio
+    {
+        private string nombre;
+        private int dni;
+
+        public string Nombre 
+        { 
+            set 
+            { 
+                nombre = value; 
+            }
+            get
+            { 
+                return nombre; 
+            }
+        }
+        public int Dni
+        {
+            set
+            {
+                dni = value;
+            }
+            get
+            {
+                return dni;
+            }
+        }
+       
+        
+    }
+    class Profesor : PersonaGimnasio 
+    {
+        private string especialidad;
+        
+        public string Especialidad
+        {
+            set
+            {
+                especialidad = value;
+            }
+            get
+            {
+                return especialidad;
+            }
+        }
+    }
+    internal class Gym
+    {
+        private PersonaGimnasio persona1, persona2;
+        private Profesor profesor;
+
+        public Gym()
+        {
+            persona1 = new PersonaGimnasio();
+            persona1.Nombre = "Pablo";
+            persona1.Dni = 49123123; 
+            persona2 = new PersonaGimnasio();
+            persona2.Nombre = "Nilt";
+            persona2.Dni = 49316121;
+            profesor = new Profesor();
+            profesor.Nombre = "Marcos";
+            profesor.Dni = 12234123;
+            profesor.Especialidad = "Programar";
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("--cargando datos de la persona--");
+            Console.WriteLine();
+            Console.WriteLine($"    Nombre de la persona {persona1.Nombre} y su dni {persona1.Dni}");
+            Console.WriteLine($"    Nombre de la persona {persona2.Nombre} y su dni {persona2.Dni}");
+            Console.WriteLine();
+            Console.WriteLine("--cargando datos de la persona--");
+            Console.WriteLine();
+            Console.WriteLine($"    Nombre del profesor {profesor.Nombre} y su dni {profesor.Dni} y su especialidad {profesor.Especialidad}");
+        }
+        static void Main(string[] args)
+        {
+
+            Gym G = new Gym();
+            G.Imprimir();
+            Console.ReadKey();
+            
+        
+            
+        }
+    }
+}
+    
+
---
+++ Act-N°17-Martin-Alanoca/Ejercicio2/Program.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Pelicula
+    {
+        private string titulo;
+        private float duracionminutos;
+        private float calificacion;
+        
+        public string Titulo
+        {
+            set 
+            { 
+                titulo = value; 
+            }
+            get
+            { 
+                return titulo; 
+            }
+        }
+        public float DuracionMinutos
+        {
+            set
+            {
+                duracionminutos = value;
+            }
+            get 
+            { 
+                return duracionminutos; 
+            }
+        }
+        public float Calificacion
+        {
+            set 
+            { 
+                calificacion = value; 
+            }
+            get
+            {
+                return calificacion;
+            }
+        }
+        public Pelicula()
+        {
+            String linea;
+            Console.Write("Ingrese el titulo de la pelicula: ");
+            linea = Console.ReadLine();
+            Titulo = linea;
+
+            Console.Write("Ingrese la duracion de la pelicula en Min: ");
+            linea = Console.ReadLine();
+            DuracionMinutos = float.Parse(linea);
+
+            Console.Write("Ingrese la calificacion: ");
+            linea = Console.ReadLine();
+            Calificacion = float.Parse(linea);
+        }
+        public void coreccion()
+        {
+            if (calificacion > 5)
+            {
+                calificacion = 1;
+            }
+        }
+    }
+    internal class Catalogo
+    {
+        private Pelicula[] pelicula;
+
+        public Catalogo()
+        {
+            pelicula = new Pelicula[3];
+            
+            for (int i = 0; i < pelicula.Length; i++)
+            {
+                pelicula[i] = new Pelicula();
+               
+            }   
+        }
+        static void Main(string[] args)
+        {
+            Catalogo Cat = new Catalogo();
+            
+            
+            for (int i = 0; i < 3; i++) 
+            {
+                Cat.pelicula[i].coreccion();
+                Console.WriteLine("calificacion: " + Cat.pelicula[i].Calificacion);
+            }
+            Console.ReadKey();
+        }
+    }
+}
