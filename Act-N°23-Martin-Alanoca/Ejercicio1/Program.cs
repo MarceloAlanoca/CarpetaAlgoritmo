7499bb277840c424ef1793dff7540829f920223f
Martin <elfalopade2024@gmail.com>
2026-08-07 11:55:45 -0300

    Trabajo a media, falta terminar

---
+++ Act-N°23-Martin-Alanoca/Ejercicio1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Ejercicio1
+{
+    /*
+        En un servidor de red, las solicitudes de impresión se procesan dinámicamente en orden de
+        llegada.
+         Crear la clase Documento que contenga como atributos privados:
+        nombreArchivo (string) y cantidadPaginas (int). Definir sus
+        propiedades de solo lectura y un constructor que reciba los parámetros nom
+        y pag.
+         Crear la clase ServidorImpresion que administre una lista dinámica de
+        documentos (List&lt;Documento&gt;).
+         Métodos en ServidorImpresion:
+        1. AgregarDocumento(): Solicitar por teclado los datos de un
+        documento y agregarlo al final de la lista utilizando .Add().
+        2. ImprimirSiguiente(): Si la lista no está vacía, simular la
+        impresión del primer documento de la lista (mostrar sus datos en
+        consola) y removerlo de la colección mediante .RemoveAt(0). Si está
+        vacía, advertir que no hay trabajos pendientes.
+        3. MostrarColaPendiente(): Listar todos los documentos que
+        están esperando ser impresos y la cantidad total de páginas
+        acumuladas en la cola de espera utilizando .Count.
+     */
+    class Documento
+    {
+        private string nombreArchivo;
+        private int cantidadPaginas;
+
+        public string NombreArchivo{
+            set
+            {
+                nombreArchivo = value;
+            }
+            get
+            {
+                return nombreArchivo;
+            }
+        }
+        public int CantidadPaginas
+        {
+            set
+            {
+                cantidadPaginas = value;
+            }
+            get
+            {
+                return cantidadPaginas;
+            }
+        }
+
+        public Documento(String nom, int pag)
+        {
+            nombreArchivo = nom;
+            cantidadPaginas= pag;
+        }
+    }
+    internal class ServidorImpresion
+    {
+        List<Documento> Documentos = new List<Documento>();
+        public void AgregarDocumento()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Ingrese el nombre del documento: ");
+                string nom = Console.ReadLine();
+
+                Console.Write("Ingrese la cantidad de paginas: ");
+                int pag = int.Parse(Console.ReadLine());
+
+                Documento Doc = new Documento(nom, pag);
+                Console.WriteLine();
+
+                Documentos.Add(Doc);
+            }
+            Console.WriteLine("se guardo correctamente en la lista");
+        }
+        public void ImprimirSiguiente()
+        {
+            if (Documentos.Count == 0) 
+            {
+                Documento doc = Documentos[0];
+                Console.WriteLine($"nombre: {doc.NombreArchivo}");
+
+                Console.WriteLine($"Cantidad de Paginas: {doc.CantidadPaginas}");
+
+                Documentos.RemoveAt(0);
+
+            }
+            else 
+            {
+                Console.WriteLine("no hay tareas pendientes");
+            }
+        }
+        public void MostrarColaPendiente()
+        {
+            
+            foreach (Documento D in Documentos)
+            {
+                Console.WriteLine($"nombre: {D.NombreArchivo}");
+
+                Console.WriteLine($"Cantidad de Paginas: {D.CantidadPaginas}");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            ServidorImpresion Si = new ServidorImpresion();
+            Si.AgregarDocumento();
+            Si.ImprimirSiguiente();
+            Si.MostrarColaPendiente();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Act-N°23-Martin-Alanoca/Ejercicio2/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ejercicio2
+{
+    /*
+        Una empresa de logística autónoma monitorea el estado y nivel de batería de sus
+        drones de entrega en vuelo de regreso a la base.
+         Crear la clase Dron que contenga los atributos privados: codigo (string) y
+        nivelBateria (int, de 0 a 100). Definir sus propiedades correspondientes.
+        Su constructor debe recibir cod y bat.
+         Crear la clase CentroControl que administre una lista de objetos
+        List&lt;Dron&gt;.
+         Métodos en CentroControl:
+
+        1. Un constructor que cargue por teclado una lista inicial de 4 drones
+        ingresando sus códigos y baterías.
+        2. ListarFlota(): Mostrar la lista de drones en pantalla.
+        3. RemoverDronesBajos(): Recorrer la lista y remover por
+        completo de la flota a todos aquellos drones cuyo nivel de batería
+        sea menor o igual al 15% (ya que requieren mantenimiento
+        automático urgente).
+        4. MostrarDronesRestantes(): Imprimir la flota actualizada y la
+        cantidad de drones operativos utilizando la propiedad .Count.
+     */
+    class Dron
+    {
+        private string codigo;
+        private int nivelBateria;
+
+        public string Codigo
+        {
+            set
+            {
+                codigo = value;
+            }
+            get
+            {
+                return codigo;
+            }
+        }
+        public int NivelBateria
+        {
+            set
+            {
+                nivelBateria = value;
+            }
+            get
+            {
+                return nivelBateria;
+            }
+        }
+        public Dron(string cod, int bat)
+        {
+            codigo = cod;
+            nivelBateria = bat;
+
+            
+        }
+       
+    }
+    internal class CentroControl
+    {
+        List<Dron> D_drom = new List<Dron>();
+        public CentroControl() 
+        {
+            Dron[] d = new Dron[3];
+
+            for (int i = 0; i < d.Length; i++) 
+            {
+                Console.Write("Ingrese el codigo del Dron: ");
+                string cod = Console.ReadLine();
+
+                Console.Write("Ingrese la bateria del Dron(Si el valor es mayor a 100 o menor a 0 el valor por defecto sera 0): ");
+                int bat = int.Parse(Console.ReadLine());
+
+                if (bat < 0) 
+                { 
+                    bat = 0;
+                }
+                if (bat > 100) 
+                {
+                    bat = 0;
+                }
+
+
+                Dron DN = new Dron(cod, bat);
+                
+                Console.WriteLine();
+                
+
+                D_drom.Add(DN);
+                
+            }
+            
+        }
+        public void ListarFlota()
+        {
+            foreach (Dron DN in D_drom)
+            {
+                
+                Console.WriteLine($"Codigo del dron: {DN.Codigo}");
+
+                Console.WriteLine($"Porcentaje de bateria del dron: {DN.NivelBateria}");
+               
+
+            }
+            Console.WriteLine();
+        }
+        public void RemoverDronesBajos()
+        {
+            /*
+            for (int i = 0; i < D_drom.Count; i++)
+            {
+                
+                
+                
+                    D_drom.RemoveAt(i);
+                    Console.WriteLine("Este dron nesecita mantenimiento");
+
+                
+            }
+            */
+            
+        }
+        public void MostrarDronesRestantes()
+        {
+
+            Console.WriteLine("===Mostrando Drones Restantes==="); 
+            foreach (Dron DN in D_drom)
+            {
+
+
+                for (int i = 0; i < D_drom.Count;  ++i) 
+                {
+                    Console.WriteLine($"Codigo del dron: {DN.Codigo}");
+
+                    Console.WriteLine($"Porcentaje de bateria del dron: {DN.NivelBateria}");
+                }
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            
+            CentroControl CC = new CentroControl();
+            
+            CC.ListarFlota();
+            CC.RemoverDronesBajos();
+            CC.MostrarDronesRestantes();
+            Console.ReadKey();  
+        }
+    }
+}
